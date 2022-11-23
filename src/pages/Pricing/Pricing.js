80e621dc45aad1fcb5dbe2148cc3ca230b561317
Labib44 <mdlabib162@gmail.com>
2022-11-24 03:42:17 +0600

    Added pricing

--- src/pages/Pricing/Pricing.js
+++ src/pages/Pricing/Pricing.js
@@ -5,7 +5,6 @@ const Pricing = () => {
 		<div>
 			<h1 className='text-center text-2xl text-primary font-bold'>Choose your best plan</h1>
 			<div className='grid mt-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-			
 			<div className="card w-full bg-emerald-300 text-primary-content">
 				<div className="card-body">
 					<h2 className="card-title">Low Price $ 99-200</h2>
